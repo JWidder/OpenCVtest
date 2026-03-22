7d31839ddcc32d9be18fa4e19a31da8b216ca5da
Johannes Widder <johannes.widder@yahoo.de>
2026-03-22 08:30:29 +0100

    cmaek und VS-Code inegriert

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hallo! Wie heißt du denn?" << endl;
+
+    string name;
+    name="Test";
+
+    cout << "Schön dich kennenzulernen, " << name << "!" << endl;
+
+    return 0;
+}
